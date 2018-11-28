feebf87d83facab97c71548cd7be3b7674c8ea44
Samuel Morarescu <walachul@gmail.com>
2018-11-28 19:38:03 +0000

    Changed the second time the whole structure of the project because data is lacking and is inconsistent from GreenPeace reports in Romania. Using data from Carbon Dioxide Information Analysis Center

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,192 +1,332 @@
 $(document).ready(function() {
+    var region = dc.selectMenu('#region');
+    var totalEmission = dc.pieChart("#total_emission");
+    var compositeChart = dc.compositeChart("#years_total_emissions");
+    var solidFuel = dc.barChart("#Emissions_from_solid_fuel_consumption");
+    var liquidFuel = dc.barChart("#Emissions_from_liquid_fuel_consumption");
+    var gasFuel = dc.barChart("#Emissions_from_gas_fuel_consumption");
+    var cementProduction = dc.barChart("#Emissions_from_cement_production");
+    var gasFlaring = dc.barChart("#Emissions_from_gas_flaring");
+
     //Using Queue.js for waiting the external data to be loaded
     queue()
-        .defer(d3.csv, "data/Romania_Deforestation_per_County.csv")
+        .defer(d3.csv, "data/Regional_fossil_fuel_emission_1751_2014.csv")
+        .defer(d3.csv, "data/Countries_Total_CO2.csv")
         .await(makeResults);
 
-    function makeResults(error, deforestationData) {
+    function makeResults(error, co2Data) {
 
-        const ndx = crossfilter(deforestationData);
+        const ndx = crossfilter(co2Data);
 
-        show_county_selector(ndx);
-        show_total_illegal_cutting_in_time(ndx);
-        show_cutting_per_county_in_years(ndx);
-        show_prejudice_per_county(ndx);
-        show_total_prejudice_in_time_period(ndx);
-        show_volume_of_wood_county(ndx);
-        /* show_medical_condition(ndx1);
-         show_improvement_percentage_per_med_cond(ndx1);
-        */
+        show_region_selector(ndx);
+        show_total_co2_emissions(ndx);
+        show_emissions_from_solid_fuel_consumption(ndx);
+        show_emissions_from_liquid_fuel_consumption(ndx);
+        show_emissions_from_gas_fuel_consumption(ndx);
+        show_emissions_from_cement_production(ndx);
+        show_emissions_from_gas_flaring(ndx);
+        show_total_CO2_per_year(ndx, co2Data);
+
+
         dc.renderAll();
     }
 
-    function show_county_selector(ndx) {
+    function show_region_selector(ndx) {
 
-        var countyDim = ndx.dimension(dc.pluck('County'));
-        var countyGroup = countyDim.group();
+        var regionDim = ndx.dimension(dc.pluck('Region'));
+        var regionGroup = regionDim.group();
 
-        dc.selectMenu('#county')
-            .dimension(countyDim)
-            .group(countyGroup);
-
-    }
-
-    function show_total_illegal_cutting_in_time(ndx) {
-        var yearsDim = ndx.dimension(dc.pluck('Time_period'));
-        var totalCutting = yearsDim.group().reduceSum(dc.pluck('Illegal_cutting_of_trees'));
-
-        dc.barChart("#totalCutting")
-            .width(300)
-            .height(400)
-            .dimension(yearsDim)
-            .group(totalCutting)
-            .margins({ top: 10, right: 50, bottom: 30, left: 50 })
-            .transitionDuration(500)
-            .x(d3.scale.ordinal())
-            .xUnits(dc.units.ordinal)
-            .elasticY(true)
-            .xAxisLabel("Total cutting of trees between 2009-2016")
-            .yAxis().ticks(10);
-    }
-
-    function show_cutting_per_county_in_years(ndx) {
-        var countyDim = ndx.dimension(dc.pluck('County'));
-        var countyGroup = countyDim.group().reduceSum(dc.pluck('Illegal_cutting_of_trees'));
-
-        dc.barChart("#total_trees_per_county")
-            .width(1400)
-            .height(300)
-            .dimension(countyDim)
-            .group(countyGroup)
-            .margins({ top: 10, right: 50, bottom: 30, left: 50 })
-            .transitionDuration(800)
-            .x(d3.scale.ordinal())
-            .xUnits(dc.units.ordinal)
-            .yAxisLabel("Total cutting of trees per county")
-            .xAxisLabel("Counties in Romania where illegally cutting of trees occured")
-            .yAxis().ticks(10);
+        region
+            .dimension(regionDim)
+            .group(regionGroup);
 
     }
 
-    function show_prejudice_per_county(ndx) {
-        var countyDim = ndx.dimension(dc.pluck('County'));
-        var totalPrejudiceCountyGroup = countyDim.group().reduceSum(dc.pluck('Prejudice_per_county'));
-        var filtertotalPrejudiceCountyGroup = {
-            all: function() {
-                return totalPrejudiceCountyGroup.top(Infinity).filter(function(d) { return d.Prejudice_per_county !== 0; })
-            }
-        };
+    function show_total_co2_emissions(ndx) {
+        var regionDim = ndx.dimension(dc.pluck('Region'));
+        var totalEmissionGroup = regionDim.group().reduceSum(dc.pluck('Total_CO2_emissions_from_fossil_fuels_and_cement_prod'));
 
-        dc.pieChart('#prejudicePerCounty')
-            .width(400)
+        totalEmission
+            .width(525)
             .height(300)
             .transitionDuration(1500)
-            .legend(dc.legend().x(5).y(20).itemHeight(15).gap(5))
-            .dimension(countyDim)
-            .group(totalPrejudiceCountyGroup)
-            .group(filtertotalPrejudiceCountyGroup);
+            .legend(dc.legend().x(-18).y(20).itemHeight(15).gap(7))
+            .innerRadius(20)
+            .dimension(regionDim)
+            .group(totalEmissionGroup);
     }
 
-    function show_total_prejudice_in_time_period(ndx) {
-        var timeDim = ndx.dimension(dc.pluck('Time_period'));
-        var totalPrejudice = timeDim.group().reduceSum(dc.pluck('Total_prejudice'));
-
-
-        dc.rowChart('#totalPeriodPrejudice')
-            .width(400)
-            .height(150)
-            .transitionDuration(1500)
-            .dimension(timeDim)
-            .group(totalPrejudice);
-
-
-    }
-
-    function show_volume_of_wood_county(ndx) {
-        var countyDim = ndx.dimension(dc.pluck('County'));
-        var woodGroup = countyDim.group().reduce(reduceAdd, reduceRemove, reduceInitial);
-
-        function reduceAdd(p, v) {
-            ++p.count;
-            p.total += v.Wood_volume_illegaly_cut;
-            return p;
-        }
-
-        function reduceRemove(p, v) {
-            --p.count;
-            p.total -= v.Wood_volume_illegaly_cut;
-            return p;
-        }
-
-        function reduceInitial() {
-            return { count: 0, total: 0 };
-        }
-        woodGroup.top(Infinity).filter(function(d){
-            return d.Wood_volume_illegaly_cut > 0;
+    function show_total_CO2_per_year(ndx, co2Data) {
+        var parseDate = d3.time.format("%Y").parse;
+        co2Data.forEach(function(d){
+            d.Year = parseDate(d.Year);
         });
-        dc.rowChart("#woodVolumePerCounty")
-            .width(700)
+        var yearDim = ndx.dimension(dc.pluck('Year'));
+        var minDate = yearDim.bottom(1)[0].Year;
+        var maxDate = yearDim.top(1)[0].Year;
+        var totalEmissionGroup = yearDim.group().reduceSum(dc.pluck('Total_CO2_emissions_from_fossil_fuels_and_cement_prod'));
+
+        var africaTotalEmissionsinYears = yearDim.group().reduceSum(function(d) {
+            if (d.Region === 'Africa') {
+                return +d.Total_CO2_emissions_from_fossil_fuels_and_cement_prod;
+            }
+            else {
+                return 0;
+            }
+        });
+        var developingAmericaTotalEmissionsinYears = yearDim.group().reduceSum(function(d) {
+            if (d.Region === 'Developing America') {
+                return +d.Total_CO2_emissions_from_fossil_fuels_and_cement_prod;
+            }
+            else {
+                return 0;
+            }
+        });
+        var asiaTotalEmissionsinYears = yearDim.group().reduceSum(function(d) {
+            if (d.Region === 'Centrally Planned Asia') {
+                return +d.Total_CO2_emissions_from_fossil_fuels_and_cement_prod;
+            }
+            else {
+                return 0;
+            }
+        });
+        var europeTotalEmissionsinYears = yearDim.group().reduceSum(function(d) {
+            if (d.Region === 'Centrally Planned Europe') {
+                return +d.Total_CO2_emissions_from_fossil_fuels_and_cement_prod;
+            }
+            else {
+                return 0;
+            }
+        });
+        var farEastTotalEmissionsinYears = yearDim.group().reduceSum(function(d) {
+            if (d.Region === 'Far East') {
+                return +d.Total_CO2_emissions_from_fossil_fuels_and_cement_prod;
+            }
+            else {
+                return 0;
+            }
+        });
+        var germanyTotalEmissionsinYears = yearDim.group().reduceSum(function(d) {
+            if (d.Region === 'Germany') {
+                return +d.Total_CO2_emissions_from_fossil_fuels_and_cement_prod;
+            }
+            else {
+                return 0;
+            }
+        });
+        var middleEastTotalEmissionsinYears = yearDim.group().reduceSum(function(d) {
+            if (d.Region === 'Middle East') {
+                return +d.Total_CO2_emissions_from_fossil_fuels_and_cement_prod;
+            }
+            else {
+                return 0;
+            }
+        });
+        var northAmericaTotalEmissionsinYears = yearDim.group().reduceSum(function(d) {
+            if (d.Region === 'North America') {
+                return +d.Total_CO2_emissions_from_fossil_fuels_and_cement_prod;
+            }
+            else {
+                return 0;
+            }
+        });
+        var oceaniaTotalEmissionsinYears = yearDim.group().reduceSum(function(d) {
+            if (d.Region === 'Oceania') {
+                return +d.Total_CO2_emissions_from_fossil_fuels_and_cement_prod;
+            }
+            else {
+                return 0;
+            }
+        });
+        var westernEuropeTotalEmissionsinYears = yearDim.group().reduceSum(function(d) {
+            if (d.Region === 'Western Europe') {
+                return +d.Total_CO2_emissions_from_fossil_fuels_and_cement_prod;
+            }
+            else {
+                return 0;
+            }
+        });
+
+        compositeChart
+            .width(800)
+            .height(300)
+            .transitionDuration(1500)
+            .margins({ top: 10, right: 10, bottom: 40, left: 62 })
+            .dimension(yearDim)
+            .elasticX(true)
+            .xAxisPadding(50)
+            .brushOn(false)
+            .x(d3.time.scale().domain([minDate, maxDate]))
+            .yAxisLabel("Total Emissions per Region")
+            .legend(dc.legend().x(80).y(20).itemHeight(13).gap(5))
+            .renderHorizontalGridLines(true)
+            .compose([
+                dc.barChart(compositeChart)
+                .colors('yellow')
+                .group(africaTotalEmissionsinYears, 'Africa'),
+                dc.barChart(compositeChart)
+                .colors('blue')
+                .group(developingAmericaTotalEmissionsinYears, 'Developing America'),
+                dc.barChart(compositeChart)
+                .colors('purple')
+                .group(asiaTotalEmissionsinYears, 'Asia'),
+                dc.barChart(compositeChart)
+                .colors('orange')
+                .group(europeTotalEmissionsinYears, 'Europe'),
+                dc.barChart(compositeChart)
+                .colors('brown')
+                .group(farEastTotalEmissionsinYears, 'Far East'),
+                dc.barChart(compositeChart)
+                .colors('black')
+                .group(germanyTotalEmissionsinYears, 'Germany'),
+                dc.barChart(compositeChart)
+                .colors('black')
+                .group(middleEastTotalEmissionsinYears, 'Middle East'),
+                dc.barChart(compositeChart)
+                .colors('steelblue')
+                .group(northAmericaTotalEmissionsinYears, 'North America'),
+                dc.barChart(compositeChart)
+                .colors('skyblue')
+                .group(oceaniaTotalEmissionsinYears, 'Oceania'),
+                dc.barChart(compositeChart)
+                .colors('purple')
+                .group(westernEuropeTotalEmissionsinYears, 'Western Europe'),
+            ])
+            .brushOn(false)
+            .renderLabel(true);
+    }
+
+    function show_emissions_from_solid_fuel_consumption(ndx) {
+        var regionDim = ndx.dimension(dc.pluck('Region'));
+        var solidFuelGroup = regionDim.group().reduceSum(dc.pluck('Emissions_from_solid_fuel_consumption'));
+
+        solidFuel
+            .width(500)
             .height(400)
-            .transitionDuration(800)
-            .dimension(countyDim)
-            .group(woodGroup);
-
-    }
-    /*
-    function show_medical_condition(ndx1) {
-        var groupsDim = ndx1.dimension(dc.pluck('Groups'));
-        var groupG = groupsDim.group();
-
-        dc.selectMenu("#medicalCondition")
-            .dimension(groupsDim)
-            .group(groupG);
-    }
-
-    function show_improvement_percentage_per_med_cond(ndx) {
-        var group_dim = ndx.dimension(dc.pluck('Dependent_variable'));
-        var improveGroup = group_dim.group();
-        
-
-        /* var stackedChart = dc.barChart("#results2");
-         stackedChart
-             .width(800)
-             .height(400)
-             .transitionDuration(1000)
-             .dimension(groupsDim)
-             .group(reikiGroup, "Reiki")
-             .stack(pmrGroup, "PMR", function(d){
-                 return d.value;
-             })
-             .stack(controlGroup, "Control", function(d){
-                 return d.value;
-             })
-             .stack(placeboGroup, "Placebo", function(d){
-                 return d.value;
-             })
-             .legend(dc.legend().x(200).y(40).itemHeight(13).gap(5))
-             .x(d3.scale.linear().domain([0, 100]))
-             .brushOn(false)
-             .renderLabel(true)
-             .xUnits(dc.units.ordinal)
-             .xAxisLabel("Medical condition")
-             .yAxisLabel("Improvement");
-             
-        dc.barChart('#results2')
-            .width(460)
-            .height(100)
-            .margins({
-            top: 10,
-            right: 10,
-            bottom: 30,
-            left: 30})
-            .transitionDuration(1000)
-            .dimension(group_dim)
-            .group(improveGroup)
+            .dimension(regionDim)
+            .group(solidFuelGroup)
+            .margins({ top: 20, right: 60, bottom: 80, left: 70 })
+            .transitionDuration(2500)
             .x(d3.scale.ordinal())
             .xUnits(dc.units.ordinal)
-            .xAxisLabel("Medical Condition")
-            .yAxis().ticks(4);
+            .yAxisLabel("Total emissions from solid fuel consumption")
+            .yAxis().ticks(16)
+
+        solidFuel.on('pretransition', function(chart) {
+            // Rotate X-axis
+            chart.selectAll("g.axis.x text")
+                .style("text-anchor", "start")
+                .attr("dx", "0.2em")
+                .attr("dy", "0.4em")
+                .attr("transform", "rotate(25)")
+        });
 
     }
-    */
+
+    function show_emissions_from_liquid_fuel_consumption(ndx) {
+        var regionDim = ndx.dimension(dc.pluck('Region'));
+        var liquidFuelGroup = regionDim.group().reduceSum(dc.pluck('Emissions_from_liquid_fuel_consumption'));
+
+        liquidFuel
+            .width(500)
+            .height(400)
+            .dimension(regionDim)
+            .group(liquidFuelGroup)
+            .margins({ top: 20, right: 60, bottom: 80, left: 70 })
+            .transitionDuration(1500)
+            .x(d3.scale.ordinal())
+            .xUnits(dc.units.ordinal)
+            .yAxisLabel("Total emissions from liquid fuel consumption ")
+            .yAxis().ticks(16)
+        liquidFuel.on('pretransition', function(chart) {
+            // Rotate X-axis
+            chart.selectAll("g.axis.x text")
+                .style("text-anchor", "start")
+                .attr("dx", "0.2em")
+                .attr("dy", "0.4em")
+                .attr("transform", "rotate(25)")
+        });
+
+    }
+
+    function show_emissions_from_gas_fuel_consumption(ndx) {
+        var regionDim = ndx.dimension(dc.pluck('Region'));
+        var gasFuelGroup = regionDim.group().reduceSum(dc.pluck('Emissions_from_gas_fuel_consumption'));
+
+        gasFuel
+            .width(500)
+            .height(400)
+            .dimension(regionDim)
+            .group(gasFuelGroup)
+            .margins({ top: 20, right: 60, bottom: 80, left: 70 })
+            .transitionDuration(1500)
+            .x(d3.scale.ordinal())
+            .xUnits(dc.units.ordinal)
+            .yAxisLabel("Total emissions from liquid fuel consumption ")
+            .yAxis().ticks(16)
+        gasFuel.on('pretransition', function(chart) {
+            // Rotate X-axis
+            chart.selectAll("g.axis.x text")
+                .style("text-anchor", "start")
+                .attr("dx", "0.2em")
+                .attr("dy", "0.4em")
+                .attr("transform", "rotate(25)")
+        });
+
+    }
+
+    function show_emissions_from_cement_production(ndx) {
+        var regionDim = ndx.dimension(dc.pluck('Region'));
+        var cementGroup = regionDim.group().reduceSum(dc.pluck('Emissions_from_cement_production'));
+
+        cementProduction
+            .width(500)
+            .height(400)
+            .dimension(regionDim)
+            .group(cementGroup)
+            .margins({ top: 20, right: 60, bottom: 80, left: 70 })
+            .transitionDuration(1500)
+            .x(d3.scale.ordinal())
+            .xUnits(dc.units.ordinal)
+            .yAxisLabel("Total emissions from cement production per region")
+            .yAxis().ticks(16)
+        cementProduction.on('pretransition', function(chart) {
+            // Rotate X-axis
+            chart.selectAll("g.axis.x text")
+                .style("text-anchor", "start")
+                .attr("dx", "0.2em")
+                .attr("dy", "0.4em")
+                .attr("transform", "rotate(25)")
+        });
+
+    }
+
+    function show_emissions_from_gas_flaring(ndx) {
+        var regionDim = ndx.dimension(dc.pluck('Region'));
+        var gasFlaringGroup = regionDim.group().reduceSum(dc.pluck('Emissions_from_gas_flaring'));
+
+        gasFlaring
+            .width(500)
+            .height(400)
+            .dimension(regionDim)
+            .group(gasFlaringGroup)
+            .margins({ top: 20, right: 60, bottom: 80, left: 70 })
+            .transitionDuration(1500)
+            .x(d3.scale.ordinal())
+            .xUnits(dc.units.ordinal)
+            .yAxisLabel("Total emissions from gas flaring per region")
+            .yAxis().ticks(16)
+        gasFlaring.on('pretransition', function(chart) {
+            // Rotate X-axis
+            chart.selectAll("g.axis.x text")
+                .style("text-anchor", "start")
+                .attr("dx", "0.2em")
+                .attr("dy", "0.4em")
+                .attr("transform", "rotate(25)")
+        });
+
+    }
+
+
+
 });
